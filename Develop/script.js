b71e693b853ddc38b6b46914a606973238abbeca
Your Name <Vaaax@Jon-PC>
2022-01-10 22:21:49 -0600

    bug occured in the JS

--- Develop/script.js
+++ Develop/script.js
@@ -2,4 +2,21 @@
 var today = moment().format('LLLL');
 $('#currentDay').html(today);
 
-// Time Rows
+// color code for each timeblock
+function background () {
+    var currentHour = moment().hours();
+    $(".time-block").each(function () {
+        var divTimeId = parseInt($(this).attr("id"));
+        if (divTimeId < currentHour) {
+            $(this).addClass("past");
+        } else if (divTimeId === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+    }
+}
